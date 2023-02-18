4ced0638c4f74df9fa8e79a54d94a8d59305d610
IMRANDIL <aliimranadil2@gmail.com>
2023-02-18 22:05:32 +0530

    some more go

--- main.go
+++ main.go
@@ -75,10 +75,11 @@ func createMovie(w http.ResponseWriter, r *http.Request) {
 }
 
 func updateMovie (w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type","application/json")
 	params := mux.Vars(r)
 	for index, item := range movies {
 		if item.ID == params["id"] {
-			
+
 		}
 	}
 }
