62e05fa548f398d3bee24af384f93ac8a2ccd66c
IMRANDIL <aliimranadil2@gmail.com>
2023-02-18 20:48:01 +0530

    some more added go go

--- main.go
+++ main.go
@@ -45,6 +45,18 @@ func deleteMovie (w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for _, item := range movies {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			break
+		}
+	}
+}
+
 func main() {
 	r := mux.NewRouter()
 
