9f0ed476a054c4c228c5f322b4715cd634471957
IMRANDIL <aliimranadil2@gmail.com>
2023-02-18 22:12:58 +0530

    added the edit movie too

--- main.go
+++ main.go
@@ -79,7 +79,13 @@ func updateMovie (w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	for index, item := range movies {
 		if item.ID == params["id"] {
-
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = strconv.Itoa(rand.Intn(100000000))
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+			return
 		}
 	}
 }
